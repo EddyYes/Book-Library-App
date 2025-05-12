e976f7c3bc9ecf0825feae1f755dd60417dcae28
Eduard Filonov <krovli.da@gmail.com>
2025-05-12 19:54:18 +0300

    Filter by author

--- frontend/src/redux/slices/filterSlice.js
+++ frontend/src/redux/slices/filterSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   title: '',
+  author: '',
 }
 
 const filterSlice = createSlice({
@@ -14,14 +15,22 @@ const filterSlice = createSlice({
       // return { ...state, title: action.payload }
       // 2-й вариант-традиционный подход
     },
+    setAuthorFilter: (state, action) => {
+      //1-й вариант использование Immer
+      state.author = action.payload
+      // return { ...state, author: action.payload }
+      // 2-й вариант-традиционный подход
+    },
     resetFilters: (state) => {
       return initialState
     },
   },
 })
 
-export const { setTitleFilter, resetFilters } = filterSlice.actions
+export const { setTitleFilter, setAuthorFilter, resetFilters } =
+  filterSlice.actions
 
 export const selectTitleFilter = (state) => state.filter.title
+export const selectAuthorFilter = (state) => state.filter.author
 
 export default filterSlice.reducer
